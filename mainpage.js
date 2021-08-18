ec86ea6bc58136ae92b665184f3a35691627e0c9
Rajanjor <88129982+Rajanjor@users.noreply.github.com>
2021-08-18 20:40:10 +0530

    dwefdew

---
+++ mainpage.js
@@ -0,0 +1,4 @@
+var roomKey = document.getElementById('roomKey');
+var secret = document.getElementById('secret');
+var createChat = document.getElementById('createChat');
+var signOut = document.getElementById('signOut');
