1b4ac30ca367e5b993fb9843a33cb4db8f3122f1
Rajanjor <81916900+123452123-codr@users.noreply.github.com>
2021-08-23 14:36:35 +0530

    dffgrr

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ var password = document.getElementById('password');
 var signUp = document.getElementById('signUp');
 var alreadyauser = document.getElementById('alreadyauser');
 
-signUp.addEventListener("click", function(){
-    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-    .then((userCredential) => {
-        var user = userCredential.user;
-        window.location.replace("signIn.html","_self"); 
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-        console.log(errorCode);
-    });
+signUp.addEventListener("click", e => {
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    window.location.replace("signIn.html","_self");
+    promise.catch(e => alert(e.message));
 });
 
 alreadyauser.addEventListener("click", function(){
